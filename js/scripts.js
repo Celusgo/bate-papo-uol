ec832a85a91526fe9be9eab3b322e7363a736153
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-04-15 11:42:33 -0300

    Implementação do setInterval para saber se o usuário ainda estána sala.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+let conversasServidor=[];
+
 //Entrar na sala
 const nomeUsuario = prompt("Escreva seu nome:");
 const nomeEnviado = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants',{name: nomeUsuario});
@@ -12,5 +14,31 @@ function sucessoLogin(sucesso){
 function nomeExistente(existente){
     while(existente.response.status === 400){
        nomeUsuario = prompt("Já há alguém com esse nome. Insira seu nome acompanhado do seu sobrenome."); 
+    }
+}
+//
+
+//Se manter na sala
+function statusUsuario(){
+    axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status',{name: nomeUsuario});
+}
+setInterval(statusUsuario, 5000);
+//
+
+//Carregar mensagens da sala
+const mensagensServidor = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages');
+mensagensServidor.then(mensagensRecebidas);
+
+const room = document.querySelector('.room');
+
+function mensagensRecebidas(retorna){
+    conversasServidor = retorna.data;
+    for(let i=0; i < conversasServidor.length; i++){
+        room.innerHTML +=`
+        <div class="conversa">
+            (${conversasServidor[i].time})
+            ${conversasServidor[i].from} para ${conversasServidor[i].to}: ${conversasServidor[i].text}
+        </div>
+        `
     }
 }
