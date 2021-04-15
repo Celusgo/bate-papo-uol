ed11c4bad7ec4bfd4c37c2943378ae76bfd16bf5
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-04-15 20:43:21 -0300

    Coloquei as mensagens privadas com meu nome para serem vistas apenas por mim e o botão para enviar a mensagem do input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,49 @@
+//Variáveis Globais//
 let conversasServidor=[];
 let nomeUsuario;
+//
 
 //Entrar na sala
 function perguntaNome(){
     nomeUsuario = prompt("Escreva seu nome:");
+    
+    while(nomeUsuario === ""){
+        nomeUsuario = prompt("Escreva seu nome:");
+    }
+
+    const nomeEnviado = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants',{name: nomeUsuario});
+    nomeEnviado.then(sucessoLogin);
+    nomeEnviado.catch(nomeExistente);
+
+    function sucessoLogin(){
+        alert("Seja bem vindo, " + nomeUsuario);
+        carregarMensagens();
+    }
+
+    function nomeExistente(existente){
+        if(existente.response.status === 400){
+        perguntaNome();       
+        }
+    }
 }
 perguntaNome();
 
-const nomeEnviado = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants',{name: nomeUsuario});
-nomeEnviado.then(sucessoLogin);
-nomeEnviado.catch(nomeExistente);
 
-function sucessoLogin(sucesso){
-    const sucessoStatus = sucesso.status;
-    console.log(sucessoStatus)
-}
-
-function nomeExistente(existente){
-    if(existente.response.status === 400){
-       perguntaNome();       
-    }
-}
 //
 
 //Se manter na sala
 function statusUsuario(){
-    axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status',{name: nomeUsuario});
+    const manter = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status',{name: nomeUsuario});
+    manter.catch(recarregaPagina);
 }
 setInterval(statusUsuario, 5000);
+
+function recarregaPagina(){
+    window.location.reload();
+}
 //
+
+
 
 //Carregar mensagens da sala
 function carregarMensagens(){
@@ -43,7 +58,7 @@ function mensagensRecebidas(retorna){
     for(let i=0; i < conversasServidor.length; i++){
         if(conversasServidor[i].type == "message"){
             room.innerHTML +=`
-        <div class="conversa">
+        <div class="talk">
             <p><span class='message-time'>(${conversasServidor[i].time})</span>
             <span class='username'>${conversasServidor[i].from}</span> <span class='user-message'>para</span> <span class='username'>${conversasServidor[i].to}</span>: <span class='user-message'>${conversasServidor[i].text}</span></p>
         </div>
@@ -57,9 +72,35 @@ function mensagensRecebidas(retorna){
         </div>
         `
         }
-        document.body.scrollTop = document.body.scrollHeight;
-        document.documentElement.scrollTop = document.documentElement.scrollHeight;       
+        else if (conversasServidor[i].type == "private_message" && conversasServidor[i].to === nomeUsuario){
+            room.innerHTML +=`
+        <div class="private">
+            <p><span class='message-time'>(${conversasServidor[i].time})</span>
+            <span class='username'>${conversasServidor[i].from}</span> <span class='user-message'>reservadamente para</span> <span class='username'>${conversasServidor[i].to}</span>: <span class='user-message'>${conversasServidor[i].text}</span></p>
+        </div>
+        `
+        }
+        //document.body.scrollTop = document.body.scrollHeight;
+        //document.documentElement.scrollTop = document.documentElement.scrollHeight;       
     }
 }
 }
 setInterval(carregarMensagens, 3000);
+//
+
+function enviarMensagem(){
+    const mensagem = document.querySelector("input");
+
+    const mensagemModelo = {
+        from: nomeUsuario,
+        to: "Todos",
+        text: mensagem.value,
+        type: "message"
+    }
+    const mensagemPronta = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", mensagemModelo);
+    mensagemPronta.catch(recarregaPagina);
+
+    mensagem.value = "";
+    carregarMensagens();
+}
+
